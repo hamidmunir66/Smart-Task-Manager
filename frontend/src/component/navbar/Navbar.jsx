d3e10b9bd9a13ec0d9305620cc26d1281128ad90
Hamid Munir <munirhamid66@gmail.com>
2026-02-24 16:01:12 +0500

    folder initlized,packages install

---
+++ frontend/src/component/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <header>
+        <nav className='flex '>
+
+        </nav>
+    </header>
+    </>
+  )
+}
+
+export default Navbar
